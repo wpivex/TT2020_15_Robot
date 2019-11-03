93866568b4e30d5a2b6158ef1d9d777d6cd1bfcb
MargaretEarnest <molly7312000@gmail.com>
2019-11-03 14:16:46 -0500

    Added encoder units, chassis builder, and arcade drive back in

--- src/Drivetrain.cpp
+++ src/Drivetrain.cpp
@@ -1,12 +1,12 @@
 #include "Drivetrain.hpp"
 
 Drivetrain::Drivetrain() {
-  leftDriveMotors = {okapi::Motor(DRIVE_LEFT_FRONT_PORT,false,okapi::AbstractMotor::gearset::green),
-                      okapi::Motor(DRIVE_LEFT_BACK_PORT,false,okapi::AbstractMotor::gearset::green),
-                      okapi::Motor(DRIVE_LEFT_MID_PORT,true,okapi::AbstractMotor::gearset::green)};
-   rightDriveMotors = {okapi::Motor(DRIVE_RIGHT_FRONT_PORT,true,okapi::AbstractMotor::gearset::green),
-                      okapi::Motor(DRIVE_RIGHT_BACK_PORT,true,okapi::AbstractMotor::gearset::green),
-                      okapi::Motor(DRIVE_RIGHT_MID_PORT,false,okapi::AbstractMotor::gearset::green)};
+  leftDriveMotors = {okapi::Motor(DRIVE_LEFT_FRONT_PORT,false,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees),
+                      okapi::Motor(DRIVE_LEFT_BACK_PORT,false,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees),
+                      okapi::Motor(DRIVE_LEFT_MID_PORT,true,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees)};
+   rightDriveMotors = {okapi::Motor(DRIVE_RIGHT_FRONT_PORT,true,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees),
+                      okapi::Motor(DRIVE_RIGHT_BACK_PORT,true,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees),
+                      okapi::Motor(DRIVE_RIGHT_MID_PORT,false,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees)};
 
   driveLeft = std::make_shared<okapi::MotorGroup>(leftDriveMotors);
   driveRight = std::make_shared<okapi::MotorGroup>(rightDriveMotors);
@@ -19,12 +19,20 @@ Drivetrain::Drivetrain() {
   driveLeft->setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
   driveRight->setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
 
-  chassisController = okapi::ChassisControllerFactory::createPtr(driveLeft, driveRight, leftEnc, rightEnc,
-                                                                  AbstractMotor::gearset::green,
-                                                                  {4.125_in ,11.5_in});
+  chassisController = ChassisControllerBuilder()
+              .withMotors(driveLeft, driveRight)
+              .withGearset(AbstractMotor::gearset::green)
+              .withDimensions({{4.125_in ,11.5_in},imev5GreenTPR})
+              .withGains(
+                  {0.001, 0, 0.0001}, // Distance controller gains
+                  {0.001, 0, 0.0001}, // Turn controller gains
+                  {0.001, 0, 0.0001}  // Angle controller gains (helps drive straight)
+              )
+              .withOdometry(StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps)
+              .buildOdometry();
 }
 
 void Drivetrain::drive(pros::Controller &joystick) {
-  chassisController->arcade(joystick.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)/127.0,
+  this->chassisController->model().arcade(joystick.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)/127.0,
                           joystick.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)/127.0, 0);
 }
